5759c901d588b64cde872a7b278cbec96fca2ed6
Hyunwook Nam <hw0knam@gmail.com>
2022-12-30 13:07:45 +0900

    fix: Member 라우터 경로 변경

--- app/api/api_router_v1.py
+++ app/api/api_router_v1.py
@@ -3,4 +3,4 @@ from fastapi import APIRouter
 from app.api.member_router import member_router
 
 api_router = APIRouter()
-api_router.include_router(router=member_router, prefix="/member", tags=["Member"])
+api_router.include_router(router=member_router, prefix="/members", tags=["Member"])
