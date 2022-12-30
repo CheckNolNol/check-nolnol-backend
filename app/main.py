99ea2960e39c2ef68af27efdc5766b772d37c8f3
Hyunwook Nam <hw0knam@gmail.com>
2022-12-30 13:03:52 +0900

    feat: CORS 헤더 추가

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from mangum import Mangum
+from starlette.middleware.cors import CORSMiddleware
 
 from app.api.api_router_v1 import api_router
 
 app = FastAPI(title="CheckNolNol API")
 app.include_router(router=api_router, prefix="/api/v1")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
