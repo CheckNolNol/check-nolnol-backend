633b050ecdbdce1573d5211355f2128cd7bfffab
Hyunwook Nam <hw0knam@gmail.com>
2022-12-30 10:39:21 +0900

    feat: Member DTO 정의
    fix #1

---
+++ app/dtos/member_dto.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+from app.dtos.base import BaseDTO
+
+
+class Gender(Enum):
+    MALE = 0
+    FEMALE = 1
+
+
+class Role(Enum):
+    MEMBER = "MEMBER"
+    ADMIN = "ADMIN"
+
+
+class State(Enum):
+    ACTIVE = "ACTIVE"
+    INACTIVE = "INACTIVE"
+    BANNED = "BANNED"
+
+
+class MemberDTO(BaseDTO):
+    id: int
+    name: str
+    nick_name: str
+    age: int
+    gender: Gender
+    attendance_count_current_month: int = 0
+    attendance_count_total: int = 0
+    role: Role = "MEMBER"
+    state: State = "ACTIVE"
---
+++ app/fixtures/member_dto_fixtures.py
@@ -0,0 +1,6 @@
+from app.dtos.member_dto import MemberDTO
+
+members = [
+    MemberDTO(id=1, name="남현욱", nick_name="hw0k", age=23, gender=0),
+    MemberDTO(id=2, name="김건", nick_name="rlarjs2020", age=23, gender=0),
+]
